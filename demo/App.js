c85d6791cb897ba374042483a909ede96b7ccf5b
KingNNT <Dev.KingNNT@gmail.com>
2020-08-29 14:38:05 +0700

    Update Detail Scr

--- demo/App.js
+++ demo/App.js
@@ -3,14 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import MenuScreen from './app/MenuScreen';
+import DetailScreen from './app/DetailScreen';
 const Stack = createStackNavigator();
 const MenuScr = MenuScreen;
+const DetailScr = DetailScreen;
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="MenuScr" component={MenuScr} />
+        <Stack.Screen name="Menu Screen" component={MenuScr} />
+        <Stack.Screen name="Detail" component={DetailScr} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ demo/app/DetailScreen.js
@@ -0,0 +1,5 @@
+const DetailScreen = () => {
+
+}
+
+export default DetailScreen;
