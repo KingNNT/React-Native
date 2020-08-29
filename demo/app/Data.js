30f4c2f73f97a0cd0ab0e22d92152cea54d813b3
KingNNT <Dev.KingNNT@gmail.com>
2020-08-29 14:26:27 +0700

    Update

--- demo/app/Data.js
+++ demo/app/Data.js
@@ -7,6 +7,41 @@ const AppData = [
       'An Iranian cyberespionage group known for targeting government, defense technology, military, and diplomacy ...',
     urlimage:
       'https://thehackernews.com/images/-9bqxt69plWs/X0jc-Y5VinI/AAAAAAAAAtU/Z1WP5r-nvhIMK9F6BMLufPG7M9eEuA-egCLcBGAsYHQ/s728-e100/iran.jpg',
+    data: [
+      {
+        value: `An Iranian cyberespionage group known for targeting government, defense technology, military, and diplomacy sectors is now impersonating journalists to approach targets via LinkedIn and WhatsApp and infect their devices with malware.`
+      },
+      {
+        value: `Detailing the new tactics of the "Charming Kitten" APT group, Israeli firm Clearsky said, "starting July 2020, we have identified a new TTP of the group, impersonating 'Deutsche Welle' and the 'Jewish Journal' using emails alongside WhatsApp messages as their main platform to approach the target and convince them to open a malicious link."`
+      },
+      {
+        value: `This development is the first time the threat actor is said to have carried out a watering hole attack through WhatsApp and LinkedIn, which also includes making phone calls to victims, Clearsky noted in a Thursday analysis.`
+      },
+      {
+        value: `After the company alerted Deutsche Welle about the impersonation and the watering hole in their website, the German broadcaster confirmed, "the reporter which Charming Kitten impersonated did not send any emails to the victim nor any other academic researcher in Israel in the past few weeks."`
+      },
+      {
+        value: `Charming Kitten (also known by aliases APT35, Parastoo, NewsBeef, and Newscaster) has been previously linked to a series of covert campaigns at least since December 2017 with an aim to steal sensitive information from human rights activists, academic researchers, and media outlets.`
+      },
+      {
+        value: `The watering hole — in this case, a malicious link embedded in the compromised Deutsche Welle domain — delivered the info-stealer malware via WhatsApp, but not before the victims were first approached via tried-and-tested social engineering methods with an intention to lure the academics to speak at an online webinar.`
+      },
+      {
+        value: `"The correspondence began with an email sent to the target, initiating a conversation," Clearsky explained. "After a short conversation with the target, the Charming Kitten attacker requests to move the conversation to WhatsApp. If the target refuses to move to WhatsApp, the attacker will send a message via a fake LinkedIn profile."`
+      },
+      {
+        value: `In one scenario, the adversary even took the step of messaging and calling a victim to gain the target's trust and subsequently walk the person through the steps of connecting to the webinar using the malicious link earlier shared in the chat.`
+      },
+      {
+        value: `Although APT35 may have picked up a new ruse, this is not the first time the Iranian hackers have used social media channels to spy on personnel of interest.`
+      },
+      {
+        value: `In a three-year-long "Operation Newscaster" uncovered by iSIGHT Partners (now owned by FireEye) in 2014, the threat actor was found to have created false Facebook accounts and a fake news website to spy on military and political leaders in the U.S., Israel, and other countries.`
+      },
+      {
+        value: `"In this campaign, we observed a willingness of the attackers to speak on the phone directly with the victim, using WhatsApp calls, and a legitimate German phone number. This TTP is uncommon and jeopardizes the fake identity of the attackers," Clearsky researchers said.`
+      }
+    ]
   },
   {
     id: '2',
--- demo/app/MenuScreen.js
+++ demo/app/MenuScreen.js
@@ -1,1 +1,78 @@
 import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+import AppData from './Data';
+const DATA = AppData;
+
+const Item = ({item}) => (
+  <TouchableOpacity onPress={() => Alert.alert(item.title)}>
+    <View style={styles.item}>
+      <View>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: item.urlimage,
+          }}
+        />
+      </View>
+
+      <View style={styles.ranger}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.subtitle}>{item.subtitle}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+const MenuScreen = () => {
+  const renderItem = ({item}) => <Item item={item} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#d1e0de',
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 16,
+  },
+  subtitle: {
+    fontSize: 12,
+  },
+  tinyLogo: {
+    height: 90,
+    width: 90,
+  },
+  ranger: {
+    padding: 5,
+  },
+});
+
+export default MenuScreen;
